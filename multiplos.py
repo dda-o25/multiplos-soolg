a2af1b4faee24d3df1e0e0c19e48c61f35938792
soolg <sol.gonzalez@iteso.mx>
2025-09-18 22:13:48 -0600

    primer intento

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,17 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Sol Valeria Gonzalez Castro
+17/09/2025
+Dados dos numeros, determinar si uno es multiplo del otro
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+Numero_1 = int(input("Introduzca un numero:"))
+Numero_2 = int(input("Introduzca otro numero:"))
 
 # Proceso
-
-
-# Salidas
-print(salida)
+if Numero_2 != 0 and Numero_1 % Numero_2 == 0: 
+    print(str(Numero_1) + " es multiplo de " + str(Numero_2))
+elif Numero_1 != 0 and Numero_2 % Numero_1 == 0:
+    print(str(Numero_2) + " es multiplo de " + str(Numero_1))
+else:
+    print("no son multiplos")
